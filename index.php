09e7dc0f284bf3a2cd0d059cf02060597cb3c6ee
halabe15 <halabe15@gmail.com>
2017-10-17 21:06:44 -0500

    creacion de input y css

--- index.php
+++ index.php
@@ -2,9 +2,39 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Calculadora</title>
   </head>
   <body>
-
+    <form class="" action="" method="">
+      <label for="" class="label">Lugar</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Comida</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Mudanza</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Material Actividades</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Material General</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Seguros</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Playeras</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Publicidad</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Cocineras</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Camiones</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Paramedico</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Extras</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Becas</label>
+      <input type="text" name="" value="" class="entrada">
+      <label for="" class="label">Vanguardia</label>
+      <input type="text" name="" value="" class="entrada">
+    </form>
   </body>
 </html>
