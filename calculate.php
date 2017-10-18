7d54ad632df7587c025011daa401fbd2909acebb
halabe15 <halabe15@gmail.com>
2017-10-17 21:15:56 -0500

    agrega variables desde POST en calculate

--- calculate.php
+++ calculate.php
@@ -6,5 +6,49 @@
   </head>
   <body>
 
+    <?php
+
+    // Si existe parametro en POST agrega a variable
+    if (isset($_POST['lugar']))
+      $lugar = $_POST['lugar'];
+
+    if (isset($_POST['comida']))
+      $comida = $_POST['comida'];
+
+    if (isset($_POST['mudanza']))
+      $mudanza = $_POST['mudanza'];
+
+    if (isset($_POST['material_actividades']))
+      $material_actividades = $_POST['material_actividades'];
+
+    if (isset($_POST['material_general']))
+      $material_general = $_POST['material_general'];
+
+    if (isset($_POST['seguros']))
+      $seguros = $_POST['seguros'];
+
+    if (isset($_POST['playeras']))
+      $playeras = $_POST['playeras'];
+
+    if (isset($_POST['cocineras']))
+      $cocineras = $_POST['cocineras'];
+
+    if (isset($_POST['camiones']))
+      $camiones = $_POST['camiones'];
+
+    if (isset($_POST['paramedico']))
+      $paramedico = $_POST['paramedico'];
+
+    if (isset($_POST['extras']))
+      $extras = $_POST['extras'];
+
+    if (isset($_POST['becas']))
+      $becas = $_POST['becas'];
+
+    if (isset($_POST['vanguardia']))
+      $vanguardia = $_POST['vanguardia'];
+
+    ?>
+
   </body>
 </html>
--- index.php
+++ index.php
@@ -8,33 +8,33 @@
   <body>
     <form class="" action="" method="">
       <label for="" class="label">Lugar</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="lugar" value="" class="entrada">
       <label for="" class="label">Comida</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="comida" value="" class="entrada">
       <label for="" class="label">Mudanza</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="mudanza" value="" class="entrada">
       <label for="" class="label">Material Actividades</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="material_actividades" value="" class="entrada">
       <label for="" class="label">Material General</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="material_general" value="" class="entrada">
       <label for="" class="label">Seguros</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="seguros" value="" class="entrada">
       <label for="" class="label">Playeras</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="playeras" value="" class="entrada">
       <label for="" class="label">Publicidad</label>
       <input type="text" name="" value="" class="entrada">
       <label for="" class="label">Cocineras</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="cocineras" value="" class="entrada">
       <label for="" class="label">Camiones</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="camiones" value="" class="entrada">
       <label for="" class="label">Paramedico</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="paramedico" value="" class="entrada">
       <label for="" class="label">Extras</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="extras" value="" class="entrada">
       <label for="" class="label">Becas</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="becas" value="" class="entrada">
       <label for="" class="label">Vanguardia</label>
-      <input type="text" name="" value="" class="entrada">
+      <input type="text" name="vanguardia" value="" class="entrada">
     </form>
   </body>
 </html>
