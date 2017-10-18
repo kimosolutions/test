b1accb7b42094a762efd4ba1d2a7478fb9df5a1f
davidzn2 <davidzn2@gmail.com>
2017-10-17 21:50:21 -0500

    Agrega bootstrap y estilos de bootstrap

--- index.php
+++ index.php
@@ -2,39 +2,75 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <title>Calculadora</title>
   </head>
   <body>
-    <form class="" action="" method="">
-      <label for="" class="label">Lugar</label>
-      <input type="text" name="lugar" value="" class="entrada">
-      <label for="" class="label">Comida</label>
-      <input type="text" name="comida" value="" class="entrada">
-      <label for="" class="label">Mudanza</label>
-      <input type="text" name="mudanza" value="" class="entrada">
-      <label for="" class="label">Material Actividades</label>
-      <input type="text" name="material_actividades" value="" class="entrada">
-      <label for="" class="label">Material General</label>
-      <input type="text" name="material_general" value="" class="entrada">
-      <label for="" class="label">Seguros</label>
-      <input type="text" name="seguros" value="" class="entrada">
-      <label for="" class="label">Playeras</label>
-      <input type="text" name="playeras" value="" class="entrada">
-      <label for="" class="label">Publicidad</label>
-      <input type="text" name="" value="" class="entrada">
-      <label for="" class="label">Cocineras</label>
-      <input type="text" name="cocineras" value="" class="entrada">
-      <label for="" class="label">Camiones</label>
-      <input type="text" name="camiones" value="" class="entrada">
-      <label for="" class="label">Paramedico</label>
-      <input type="text" name="paramedico" value="" class="entrada">
-      <label for="" class="label">Extras</label>
-      <input type="text" name="extras" value="" class="entrada">
-      <label for="" class="label">Becas</label>
-      <input type="text" name="becas" value="" class="entrada">
-      <label for="" class="label">Vanguardia</label>
-      <input type="text" name="vanguardia" value="" class="entrada">
-    </form>
+    <div class="container">
+      <center><h1>Calculadora de precios para Campamentos</h1></center>
+      <form class="form-group" action="" method="">
+        <div class="col-md-6">
+          <label for="" class="label">Lugar</label>
+          <input type="text" name="lugar" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Comida</label>
+          <input type="text" name="comida" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Mudanza</label>
+          <input type="text" name="mudanza" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Material Actividades</label>
+          <input type="text" name="material_actividades" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Material General</label>
+          <input type="text" name="material_general" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Seguros</label>
+          <input type="text" name="seguros" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Playeras</label>
+          <input type="text" name="playeras" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Publicidad</label>
+          <input type="text" name="" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Cocineras</label>
+          <input type="text" name="cocineras" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Camiones</label>
+          <input type="text" name="camiones" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Paramedico</label>
+          <input type="text" name="paramedico" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Extras</label>
+          <input type="text" name="extras" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Becas</label>
+          <input type="text" name="becas" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-6">
+          <label for="" class="label">Vanguardia</label>
+          <input type="text" name="vanguardia" value="" class="entrada form-control">
+        </div>
+        <div class="col-md-12">
+          <center><input class="btn btn-primary" type="button" name="Calcular" value="Submit"></center>
+        </div>
+      </form>
+    </div>
+
   </body>
 </html>
