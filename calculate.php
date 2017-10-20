d34552735ecac5ba436a6f35a0e194e3372308d4
halabe15 <halabe15@gmail.com>
2017-10-19 19:22:23 -0500

    Cambios en back

--- calculate.php
+++ calculate.php
@@ -60,86 +60,31 @@
 
       if (isset($_POST['min']) && $_POST['min'] != ""){
         $min = $_POST['min'];
-      } else {
-        $min = 10000;
       }
 
       if (isset($_POST['max']) && $_POST['max'] != ""){
         $max = $_POST['max'];
-      } else {
-        $max = 15000;
       }
 
       if (isset($_POST['escala']) && $_POST['escala'] != ""){
         $escala = $_POST['escala'];
-      } else {
-        $escala = 50;
       }
 
-      //Imprime valor de cada variable
-      // echo "Lugar = $".$lugar ."<br>";
-      // echo "Comida = $".$comida."<br>";
-      // echo "Mudanza = $".$mudanza."<br>";
-      // echo "Material Actividades = $".$material_actividades."<br>";
-      // echo "Material General= $".$material_general."<br>";
-      // echo "Seguros = $".$seguros ."<br>";
-      // echo "Playeras = $".$playeras."<br>";
-      // echo "Cocineras = $".$cocineras."<br>";
-      // echo "Camiones = $".$camiones."<br>";
-      // echo "Paramedico = $".$paramedico."<br>";
-      // echo "Extras = $".$extras."<br>";
-      // echo "Becas = $".$becas."<br>";
-      // echo "Vanguardia = $".$vanguardia."<br>";
-
-      $suma = 0;
-
-      foreach ($_POST as $key => $value) {
-        // Solo sume el total de cotizacion
-        if($key == "gente" || $key == "cobro" || $key == "min" || $key == "max" || $key == "escala"|| $key == "calcular" || $value == "parametro")
-          continue;
-        $suma += $value;
-      }
-
-      // Imprime Total
       echo "<h1 class='text-center'>Total de costo del camp. $".number_format($suma)."</h1><br>";
 
-      // Que esta pidiendo calcular
       if($_POST['parametro'] == "gente"){
 
-        $cobro = 0;
         $posibles = array();
 
-        // Cantidad de precios distintos
-        $tmp = 200;
-        for($i = 0;$i<$tmp;$i++){
-          $actual = ($gente * $cobro) - $suma;
-          // El precio pasa los parametros?
-          if($actual > $min && $actual < $max){
-            $posibles[$cobro] = $actual;
-          }
-          $cobro += $escala;
-        }
-
         foreach ($posibles as $key => $value)
-          echo "<div class='col-md-3'><p>Cobro: $".number_format($key).", Gano: $".number_format($value)."</p></div>";
+          echo "<div class='col-md-3'><p>Cobro: $".number_format(/* Cobro */).", Gano: $".number_format(/* Ganancia */)."</p></div>";
 
       } else if ($_POST['parametro'] == "cobro") {
 
-        $gente = 0;
         $posibles = array();
 
-        // Maximo de gente
-        $tmp = 2000;
-        for($i = 0;$i<$tmp;$i++){
-          $actual = ($i * $cobro) - $suma;
-          // El precio pasa los parametros?
-          if($actual > $min && $actual < $max){
-            $posibles[$i] = $actual;
-          }
-        }
-
         foreach ($posibles as $key => $value)
-          echo "<div class='col-md-3'><p>Gente: ".number_format($key).", Gano: $".number_format($value)."</p></div>";
+          echo "<div class='col-md-3'><p>Gente: $".number_format(/* Gente */).", Gano: $".number_format(/* Ganancia */)."</p></div>";
 
       } else
         echo "<h2 class='text-center text-warning'>No se selecciono ninguna opcion para calular</h2>";
