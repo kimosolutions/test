30bc45dbc8ef4f3e94b6e37a67bc0c7405854e8e
halabe15 <halabe15@gmail.com>
2017-10-19 19:17:44 -0500

    Cambios en bootstrap

--- index.php
+++ index.php
@@ -13,92 +13,92 @@
       <form class="form-group" action="calculate.php" method="post">
         <div class="row">
           <h3 class="text-center">Cotizacion</h3>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Lugar</label>
-            <input type="number" name="lugar" value="" class="entrada form-control">
+            <input type="number" name="lugar" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Comida</label>
-            <input type="number" name="comida" value="" class="entrada form-control">
+            <input type="number" name="comida" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Mudanza</label>
-            <input type="number" name="mudanza" value="" class="entrada form-control">
+            <input type="number" name="mudanza" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Material Actividades</label>
-            <input type="number" name="material_actividades" value="" class="entrada form-control">
+            <input type="number" name="material_actividades" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Material General</label>
-            <input type="number" name="material_general" value="" class="entrada form-control">
+            <input type="number" name="material_general" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Seguros</label>
-            <input type="number" name="seguros" value="" class="entrada form-control">
+            <input type="number" name="seguros" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Playeras</label>
-            <input type="number" name="playeras" value="" class="entrada form-control">
+            <input type="number" name="playeras" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Publicidad</label>
-            <input type="number" name="publicidad" value="" class="entrada form-control">
+            <input type="number" name="" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Cocineras</label>
-            <input type="number" name="cocineras" value="" class="entrada form-control">
+            <input type="number" name="cocineras" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Camiones</label>
-            <input type="number" name="camiones" value="" class="entrada form-control">
+            <input type="number" name="camiones" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Paramedico</label>
-            <input type="number" name="paramedico" value="" class="entrada form-control">
+            <input type="number" name="paramedico" value="" class="entrada  ">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <label for="" class="label">Extras</label>
-            <input type="number" name="extras" value="" class="entrada form-control">
+            <input type="number" name="extras" value="" class="entrada  ">
           </div>
-          <div class="col-md-6">
+          <div class="">
             <label for="" class="label">Becas</label>
-            <input type="number" name="becas" value="" class="entrada form-control">
+            <input type="number" name="becas" value="" class="entrada  ">
           </div>
-          <div class="col-md-6">
+          <div class="">
             <label for="" class="label">Vanguardia</label>
-            <input type="number" name="vanguardia" value="" class="entrada form-control">
+            <input type="number" name="vanguardia" value="" class="entrada  ">
           </div>
         </div>
         <div class="row">
           <h3 class="text-center">Parametros</h3>
-          <div class="col-md-3">
+          <div class="">
             <input type="radio" name="parametro" value="gente" id="radioG">
             <label for="gente">&nbsp;¿Cuanta gente esperas?</label>
-            <input type="number" name="gente" value="" class="entrada form-control" id="gente">
+            <input type="number" name="gente" value="" class="entrada" id="gente">
           </div>
-          <div class="col-md-3">
+          <div class="">
             <input type="radio" name="parametro" value="cobro" id="radioC">
             <label for="cobro">&nbsp;¿Cuanto quieres cobrar?</label>
-            <input type="number" name="cobro" value="" class="entrada form-control" id="cobro">
+            <input type="number" name="cobro" value="" class="entrada" id="cobro">
           </div>
-          <div class="col-md-2">
+          <div class="">
             <label for="min">¿Min. ganancias?</label>
-            <input type="number" name="min" value="" class="entrada form-control" placeholder="Default $10,000">
+            <input type="number" name="min" value="" class="entrada" placeholder="">
           </div>
-          <div class="col-md-2">
+          <div class="">
             <label for="max">¿Max. ganancias?</label>
-            <input type="number" name="max" value="" class="entrada form-control" placeholder="Default $15,000">
+            <input type="number" name="max" value="" class="entrada" placeholder="">
           </div>
-          <div class="col-md-2">
+          <div class="">
             <label for="escala">¿Diferencia precios?</label>
-            <input type="number" name="escala" value="" class="entrada form-control" placeholder="Default $50">
+            <input type="number" name="escala" value="" class="entrada" placeholder="">
           </div>
         </div>
         <div class="col-md-12">
           <center>
             <hr>
-            <input class="btn btn-primary" type="submit" name="Calcular" value="Calcular">
+            <input class="btn btn-warning" type="submit" name="Calcular" value="Calcular">
           </center>
         </div>
       </form>
